4e5958c6b4dac91d4138913dee4c9279560c2ab7
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2024-08-23 14:34:28 -0400

    first version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mycloud
 Simple repo to learn how to use my old Mac as server
+
+
+# To run the app
+python app.py
+
+
+ # CURL command to test the server
+ curl -X POST http://127.0.0.1:5000/capitalize -H "Content-Type: application/json" -d '{"text": "hello world"}'
+
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/capitalize', methods=['POST'])
+def capitalize_string():
+    data = request.get_json()
+    if 'text' not in data:
+        return jsonify({'error': 'No text provided'}), 400
+    
+    text = data['text']
+    capitalized_text = text.upper()
+    return jsonify({'capitalized_text': capitalized_text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
