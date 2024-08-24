2bab6d85fdc8036425d2194891836cd8deebd65d
Nicholas Vachon <nicholasvachon@hotmail.com>
2024-08-24 20:34:06 +0200

    Added a basic web page

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/capitalize', methods=['POST'])
 def capitalize_string():
-    data = request.get_json()
-    if 'text' not in data:
-        return jsonify({'error': 'No text provided'}), 400
+    if request.is_json:
+        data = request.get_json()
+        if 'text' not in data:
+            return jsonify({'error': 'No text provided'}), 400
+        text = data['text']
+    else:
+        text = request.form['text']
     
-    text = data['text']
     capitalized_text = text.upper()
-    return jsonify({'capitalized_text': capitalized_text})
+    
+    if request.is_json:
+        return jsonify({'capitalized_text': capitalized_text})
+    else:
+        return render_template('index.html', capitalized_text=capitalized_text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8088, debug=True)
